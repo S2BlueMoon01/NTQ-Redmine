51dbd4b57000856783cc2c094e94ca991012b2e2
Ngoc Hai <s2bluemoon01@gmail.com>
2024-07-09 16:45:00 +0700

    feat(): config axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/constants/config.ts
@@ -0,0 +1,5 @@
+const config = {
+  baseUrl: "https://redmine.ntq.solutions",
+};
+
+export default config;
