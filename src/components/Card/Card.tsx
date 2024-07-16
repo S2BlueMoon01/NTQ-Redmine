41da0f8eb4a3a9b568a099e9831fccba0ba4b435
hoangson20020212 <hoangson4567891@gmail.com>
2024-07-16 09:35:01 +0700

    feat() creat ui and function of Mypage Calendar

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import ArrowRightIcon from "~/assets/images/arrow_right.png";
+import { Issue } from "~/types/issue.type";
+import { checkDateStatus } from "~/utils/utils";
+
+const Card = ({ issue }: { issue: Issue }) => {
+  const urlIcon = checkDateStatus(issue.start_date, issue.due_date);
+
+  return (
+    <div className="pop-up z-10 flex absolute top-3 left-6 flex-col gap-y-1 p-1 min-w-[278px] min-h-[152px] border border-black bg-[#fff] text-[8.64px] opacity-0 invisible">
+      <div className="flex pb-5 items-center">
+        <img src={ArrowRightIcon} alt="" />
+        <a href="#!" className="text-[#116699]">
+          {issue.tracker.name} #{issue.id}
+        </a>
+        : {issue.subject}
+      </div>
+
+      <div className="flex items-center gap-1">
+        <span className="font-bold">Project:</span>
+        <img src={urlIcon} alt="" />
+        <a href="" className="text-[#116699]">
+          {issue.project.name}
+        </a>
+      </div>
+
+      <div className="flex gap-1">
+        <span className="font-bold">Status:</span>
+        <span>{issue.status.name}</span>
+      </div>
+
+      <div className="flex gap-1">
+        <span className="font-bold">Start date:</span>
+        <span>{issue.start_date}</span>
+      </div>
+
+      <div className="flex gap-1">
+        <span className="font-bold">Due date:</span>
+        <span>{issue.due_date}</span>
+      </div>
+
+      <div className="flex gap-1">
+        <span className="font-bold">Assignee:</span>
+        <span>{issue.assigned_to.name}</span>
+      </div>
+
+      <div className="flex gap-1">
+        <span className="font-bold">Priority:</span>
+        <span>{issue.priority.name}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
