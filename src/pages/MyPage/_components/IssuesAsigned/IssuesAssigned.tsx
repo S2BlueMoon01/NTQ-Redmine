58f4a6c1bf4c0c51eadbbc9490a16f0d3e095b43
Nguyễn Văn Quyết <chiengaphuly299@gmail.com>
2024-07-18 10:55:46 +0700

    refactor(): create component in my page

---
+++ src/pages/MyPage/_components/IssuesAsigned/IssuesAssigned.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Table from "~/components/Table";
+
+const columnNames = ["#", "Project", "Tracker", "Subject"];
+const dataTable = [
+  { "#": 122668, Project: "[Fresher]_ReactJS Fresher", Tracker: "Bug", Subject: "Lỗi Login (New)" },
+  { "#": 122640, Project: "[Fresher]_ReactJS Fresher", Tracker: "Task", Subject: "task abcd (Build)" },
+  { "#": 122638, Project: "[Fresher]_ReactJS Fresher", Tracker: "Task", Subject: "Clone Redmine 2 (New)" },
+];
+const IssuesAssigned = () => {
+  return (
+    <div>
+      <a className="text-ocean-blue font-semibold	hover:underline " href="">
+        Issues assigned to me ({dataTable && dataTable.length > 0 ? dataTable.length : 0})
+      </a>
+      <Table className="bg-slate-500 min-w-full mt-3" columnNames={columnNames} dataTable={dataTable} />
+    </div>
+  );
+};
+
+export default IssuesAssigned;
