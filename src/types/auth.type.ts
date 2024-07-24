57fd306bf3453324e587084146738758c774bcb3
Nguyễn Ngọc Hải <90823052+S2BlueMoon01@users.noreply.github.com>
2024-07-24 11:03:59 +0700

    refactor(): clean code

--- src/types/auth.type.ts
+++
@@ -1,9 +0,0 @@
-// import { SuccessResponse } from "./utils.type";
-
-// export type AuthResponse = SuccessResponse<{
-//   // TODO: Define AuthResponse
-// }>;
-
-// export type RefreshTokenReponse = SuccessResponse<{
-//   // TODO: Define RefreshTokenReponse
-// }>;
