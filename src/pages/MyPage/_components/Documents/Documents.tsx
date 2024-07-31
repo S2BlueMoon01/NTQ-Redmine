ee6fe6aacdb5cdca4a19f32f6a17a20289bf8043
Ngoc Hai <s2bluemoon01@gmail.com>
2024-07-31 16:23:47 +0700

    feat(): add remove document and latest news on my page

--- src/pages/MyPage/_components/Documents/Documents.tsx
+++ src/pages/MyPage/_components/Documents/Documents.tsx
@@ -1,5 +1,26 @@
+import { optionBlockMyPage } from "~/constants/constants";
+import { useGlobalStore } from "~/store/globalStore";
+import { removeBlockFromBoardSections } from "~/utils/utils";
+import CloseImg from "~/assets/images/close-img.png";
+
 const Documents = () => {
-  return <h2 className="text-mouse-gray">Documents</h2>;
+  const { isEditMyPage, removeBlock } = useGlobalStore((state) => ({
+    isEditMyPage: state.isEditMyPage,
+    removeBlock: state.removeBlock,
+  }));
+  const handleClose = () => {
+    const blockId = optionBlockMyPage.find((block) => block.title === "Documents")?.id || "";
+    removeBlockFromBoardSections({
+      blockId: blockId,
+    });
+    removeBlock(blockId);
+  };
+  return (
+    <div className="flex justify-between items-center ">
+      <h2 className="text-base text-mouse-gray font-bold">Documents</h2>
+      {isEditMyPage && <img className="w-fit h-fit mr-3 cursor-pointer" onClick={handleClose} src={CloseImg} alt="closeButton" />}
+    </div>
+  );
 };
 
 export default Documents;
--- src/pages/MyPage/_components/LatestNews/LatestNews.tsx
+++ src/pages/MyPage/_components/LatestNews/LatestNews.tsx
@@ -1,7 +1,24 @@
+import { optionBlockMyPage } from "~/constants/constants";
+import { useGlobalStore } from "~/store/globalStore";
+import { removeBlockFromBoardSections } from "~/utils/utils";
+import CloseImg from "~/assets/images/close-img.png";
+
 const LatestNews = () => {
+  const { isEditMyPage, removeBlock } = useGlobalStore((state) => ({
+    isEditMyPage: state.isEditMyPage,
+    removeBlock: state.removeBlock,
+  }));
+  const handleClose = () => {
+    const blockId = optionBlockMyPage.find((block) => block.title === "Latest news")?.id || "";
+    removeBlockFromBoardSections({
+      blockId: blockId,
+    });
+    removeBlock(blockId);
+  };
   return (
-    <div>
-      <h2 className="text-mouse-gray">LatestNews</h2>
+    <div className="flex justify-between items-center ">
+      <h2 className="text-base text-mouse-gray font-bold">Latest News</h2>
+      {isEditMyPage && <img className="w-fit h-fit mr-3 cursor-pointer" onClick={handleClose} src={CloseImg} alt="closeButton" />}
     </div>
   );
 };
