b9292d7a486d5551c359a7d6fc382ff88debc68e
Ngoc Hai <s2bluemoon01@gmail.com>
2024-07-31 15:07:35 +0700

    fix(): remove Helmet from Documents

--- src/pages/Documents/Documents.tsx
+++ src/pages/Documents/Documents.tsx
@@ -1,17 +1,5 @@
-import { Helmet } from "react-helmet-async";
-import { useParams } from "react-router-dom";
-
 const Documents = () => {
-  const { name } = useParams();
-  return (
-    <>
-      <Helmet>
-        <title>{`Documents - ${name} - NTQ Redmine`}</title>
-        <meta name="description" content="Redmine" />
-      </Helmet>
-      <h2>Documents</h2>
-    </>
-  );
+  return <h2>Documents</h2>;
 };
 
 export default Documents;
